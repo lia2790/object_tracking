7ab172e7bad148030d66c0a32ec9f9c03dd3058d
Liana <liana.bertoni@gmail.com>
2015-11-05 17:56:08 +0100

    programma base per riconoscere i marker in un stream video e programma che crea marker per essere stampati

---
+++ aruco_essential/DETECT_MARKER/build/CMakeFiles/track_marker.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/track_marker.dir/program/detection_marker.cpp.o"
+  "CMakeFiles/track_marker.dir/src/cameraparameters.cpp.o"
+  "CMakeFiles/track_marker.dir/src/cvdrawingutils.cpp.o"
+  "CMakeFiles/track_marker.dir/src/marker.cpp.o"
+  "CMakeFiles/track_marker.dir/src/markerdetector.cpp.o"
+  "CMakeFiles/track_marker.dir/src/subpixelcorner.cpp.o"
+  "CMakeFiles/track_marker.dir/src/arucofidmarkers.cpp.o"
+  "track_marker.pdb"
+  "track_marker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/track_marker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
