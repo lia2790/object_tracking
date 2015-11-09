23b5a0412d6723ee0f8c4d656167753dc85b1b7a
Liana <liana.bertoni@gmail.com>
2015-11-09 17:46:51 +0100

    aggiunta versione finale???????i hope

---
+++ aruco_essential/DETECT_MARKER_FINAL/src/cvdrawingutils.h
@@ -0,0 +1,16 @@
+
+#ifndef _ArUco_DrawUtils_H_
+#define _ArUco_DrawUtils_H_
+#include "include_src.h"
+namespace mid {
+/**\brief A set of functions to draw in opencv images
+ */
+class CvDrawingUtils {
+  public:
+    static void draw3dAxis(cv::Mat &Image, Marker &m, const CameraParameters &CP);
+
+    static void draw3dCube(cv::Mat &Image, Marker &m, const CameraParameters &CP, bool setYperpendicular = false);
+};
+};
+
+#endif
